3091e494180965501aac56a95a022fd426b09c4e
Shantanu164 <shantanushete164@gmail.com>
2025-11-09 23:40:04 +0530

    First Commit

---
+++ ProdInvApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProdinvappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ProdInvApp'
---
+++ ProdInvApp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    pid = models.IntegerField(unique=True)
+    pname = models.CharField(max_length=50)
+    category = models.CharField(max_length=30)
+    price = models.IntegerField()
+    stock_qty = models.IntegerField()
+    brand = models.CharField(max_length=50)
+    supplier = models.CharField(max_length=50)
+    purchase_date = models.DateField()
+    location = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.pid}, {self.pname}, {self.price}"
